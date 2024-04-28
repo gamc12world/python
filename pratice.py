b29cd0ed0e1ea2219d6bbefc7ee2fda85104ceb5
gamc12world <sbk520831@gmail.com>
2024-04-28 22:48:39 +0545

    added_rock_paper_scissor_game

--- pratice.py
+++ pratice.py
@@ -1,1 +1,17 @@
-print("hello world")
+import random
+def rock_paper_scissors():
+    player_input=input("your choice")
+    choice=["rock","paper","scissor"]
+    computer=random.choice(choice)
+    print(computer)
+    while computer:
+        if player_input=="rock" and computer=="scissor":
+            print("you won")
+        elif player_input=="paper" and computer=="rock":
+            print("you won")
+        elif player_input=="scissor" and computer=="paper":
+            print("you won")
+        else:
+            print("you lose and computer won")
+        break
+rock_paper_scissors()
